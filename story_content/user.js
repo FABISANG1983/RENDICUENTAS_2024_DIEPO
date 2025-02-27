107e99e4d8c2b0553d1912a5b70464278fcd6914
FABISANG1983 <fabian.sanguinetti@policia.edu.co>
2025-02-27 09:15:26 -0500

    Ajuste_02-27-2025

--- story_content/user.js
+++ story_content/user.js
@@ -5,4 +5,46 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+window.Script20 = function()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+};
+
+}
+
+window.Script21 = function()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+};
+
+}
+
 };
