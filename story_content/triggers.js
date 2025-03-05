3f86bd60223fbfc8f970e314c8a7ae3c61c02124
FABISANG1983 <115259378+FABISANG1983@users.noreply.github.com>
2025-03-05 07:18:45 -0500

    actualizacion final

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,86 +2,92 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6nQEFGfldys":
+      case "6RhG8Gct7lb":
         Script1();
         break;
-      case "69QUeH0nLnQ":
+      case "5io4eFkeLSK":
         Script2();
         break;
-      case "6G9tArEmDek":
+      case "6BzUFk0wlM6":
         Script3();
         break;
-      case "6TpOL5i3VrD":
+      case "5ueAh4gfRmx":
         Script4();
         break;
-      case "6iNZvKohigO":
+      case "5qjzpULnQOP":
         Script5();
         break;
-      case "6kn0YXFpd4B":
+      case "5nxs81rF4Gs":
         Script6();
         break;
-      case "5lZqcHc8Goq":
+      case "6IQrRZ1XjPY":
         Script7();
         break;
-      case "6gbHMx5bnGL":
+      case "5c15G6PE7ji":
         Script8();
         break;
-      case "67jQ69ycyLi":
+      case "5rUjDSpmSxy":
         Script9();
         break;
-      case "6NCJwfbrCwa":
+      case "5pHKCdRsajW":
         Script10();
         break;
-      case "6D5lQHMdMDF":
+      case "6eCCKdmf0mH":
         Script11();
         break;
-      case "5vxygGcWnjj":
+      case "6Q4WDWlTTM5":
         Script12();
         break;
-      case "66lHgfWNmF0":
+      case "5xJsQNqGAIt":
         Script13();
         break;
-      case "6lR2wzccAl3":
+      case "5ZqrLxLsDOR":
         Script14();
         break;
-      case "6RSdMz9Jk82":
+      case "6VIxj7g2IZO":
         Script15();
         break;
-      case "6cbj1n9kt0p":
+      case "5mIkCeBMNL7":
         Script16();
         break;
-      case "6V7r57ci02g":
+      case "6UYC97xTiXx":
         Script17();
         break;
-      case "6EBydW8g9UU":
+      case "6KwWBdlR0hA":
         Script18();
         break;
-      case "6f5xxZzwin7":
+      case "6rNnUjfpqwE":
         Script19();
         break;
-      case "6lS1PtO3OTZ":
+      case "6H3P5IN5933":
         Script20();
         break;
-      case "5cuxd7aGjiX":
+      case "5stTVITXqyr":
         Script21();
         break;
-      case "6W6Mfast6Zr":
+      case "6Eew6CcWA0n":
         Script22();
         break;
-      case "6PIvqlygjjJ":
+      case "6SRtXOclwOV":
         Script23();
         break;
-      case "5aVvjOKCyTc":
+      case "6AlOMfhlqf4":
         Script24();
         break;
-      case "6WX8Kc0HXfx":
+      case "6ovM0BIXsKj":
         Script25();
         break;
-      case "6aI70NtUhhN":
+      case "5hTEiSv9m77":
         Script26();
         break;
-      case "6XuyGGbgt8m":
+      case "6OTTX6EksyB":
         Script27();
+        break;
+      case "6gGhqXxgk4D":
+        Script28();
+        break;
+      case "6jwvnNjxcOi":
+        Script29();
         break;
   }
 }
--- story_content/user.js
+++ story_content/user.js
@@ -258,4 +258,46 @@ openFullscreen();
 
 }
 
+window.Script28 = function()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+};
+
+}
+
+window.Script29 = function()
+{
+  var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+
+if(document.fullscreenElement == null){
+openFullscreen();
+};
+
+}
+
 };
